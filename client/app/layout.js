01fe9875db4fd68000608972a4dedfadb572474c
Ankit Das <ad0669915@gmail.com>
2026-09-19 11:47:55 +0530

    Add metadata and RootLayout component to layout.js

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: 'Anonymous Chat',
+  description: 'Random 1-on-1 text chat',
+}
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+    </html>
+  )
+}
